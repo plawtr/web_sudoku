4068c85ef6d8df7e8138f12d48c4a0cb58c0a361
Andrey Krahmal <coolrighthere@yahoo.com>
2014-02-05 13:35:55 +0000

    Working but does not reload

--- sudoku.rb
+++ sudoku.rb
@@ -13,20 +13,91 @@ end
 
 def puzzle(sudoku)
 	some_empties = sudoku.dup
-	while some_empties.count(" ")<23  
-		some_empties[rand(80)] = " "
+	while some_empties.count("0")<23  
+		some_empties[rand(80)] = "0"
 	end
 	some_empties
 end
 
-get '/' do
+def box_order_to_row_order(cells)
+	boxes = cells.each_slice(9).to_a
+	(0..8).to_a.inject([]){|memo, i| 
+		first_box_index = i / 3 * 3
+		three_boxes = boxes[first_box_index, 3]
+		three_rows_of_three = three_boxes.map do |box|
+			row_number_in_a_box = i % 3
+			first_cell_in_the_row_index = row_number_in_a_box * 3
+			box[first_cell_in_the_row_index, 3]
+		end
+		memo+= three_rows_of_three.flatten
+	}
+end
+
+def prepare_to_check_solution
+	@check_solution = session[:check_solution]
+	session[:check_solution] = nil
+end
+
+def generate_new_puzzle_if_necessary
+	return if session[:current_solution]
 	sudoku = random_sudoku
-	session[:solution] = sudoku
-	@current_solution = puzzle(sudoku)
+	session[:solution] = sudoku 
+	session[:puzzle] = puzzle(sudoku)
+	session[:current_solution] = session[:puzzle]
+end
+
+get '/' do
+	prepare_to_check_solution
+	generate_new_puzzle_if_necessary
+	@current_solution = session[:current_solution] || session[:puzzle]
+	@solution = session[:solution]
+	@puzzle = session[:puzzle]	
+
+	puts @current_solution.inspect
+	puts @solution.inspect
+	puts @puzzle.inspect
+
 	erb :index
 end
+
+post '/' do
+	cells = params["cell"]
+	session[:current_solution] = box_order_to_row_order(cells).map{|value| value.to_i}.join
+	session[:check_solution] = true
+	redirect to("/")
+end
+
 
 get '/solution' do
 	@current_solution = session[:solution] 
 	erb :index
 end
+
+helpers do 
+	def colour_class(solution_to_check, puzzle_value, current_solution_value, solution_value)
+		# puts solution_to_check
+		# puts puzzle_value
+		# puts current_solution_value
+		# puts solution_value
+
+
+		must_be_guessed = (puzzle_value == "0")
+		tried_to_guess = (current_solution_value.to_i != 0)
+		guessed_incorrectly = (current_solution_value != solution_value)
+
+		if solution_to_check &&
+			must_be_guessed &&
+			tried_to_guess &&
+			guessed_incorrectly
+			'incorrect'
+		elsif !must_be_guessed
+			'value-provided'
+		end	
+	end
+
+	def cell_value(value)
+		value.to_i == 0 ? '' : value
+	end
+
+end
+
